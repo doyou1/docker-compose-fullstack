229e95c522008d68cd105250a557295cd5af3195
doyou1 <tla149@daum.net>
2023-08-20 02:34:46 +0900

    deploy.sh location change

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
 sudo git pull
 
+echo "deploy frontend"
 cd frontend
+
+echo "npm install"
 sudo npm install
+
+echo "npm run build"
 sudo npm run build
+
+echo "sudo rm -rf /var/www/html/*"
 sudo rm -rf /var/www/html/*
+
+echo "sudo mv ./dist/* /var/www/html/"
 sudo mv ./dist/* /var/www/html/
 
 # 재실행
+echo "sudo nginx -s reload"
 sudo nginx -s reload
 
 # my ip check
