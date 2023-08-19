6981c5674f9dafa772344da8169963d879c3f72c
doyou1 <tla149@daum.net>
2023-08-20 05:56:40 +0900

    cors4

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use*(cros({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 const todos = [
     {
